38c24b64c83317213961f8f4580f50b97fa0bb51
kheder Hassoun <103248209+kheder-hassoun@users.noreply.github.com>
2024-06-25 20:32:02 +0300

    update files

---
+++ clienttwo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static TcpClient client;
+    static NetworkStream stream;
+
+    static void Main()
+    {
+        ConnectToServer();
+        StartListeningForMessages();
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            SendMessage(message);
+        }
+    }
+
+    static void ConnectToServer()
+    {
+        client = new TcpClient("127.0.0.1", 8000); // Connect to the server's IP and port
+        stream = client.GetStream();
+        Console.WriteLine("");
+        Console.WriteLine($"am try to connected to server at {((System.Net.IPEndPoint)client.Client.RemoteEndPoint).Address}:{((System.Net.IPEndPoint)client.Client.RemoteEndPoint).Port}");
+    }
+
+    static void StartListeningForMessages()
+    {
+        Thread receiveThread = new Thread(() =>
+        {
+            while (true)
+            {
+                byte[] messageBytes = new byte[4096];
+                int bytesRead;
+
+                try
+                {
+                    bytesRead = stream.Read(messageBytes, 0, messageBytes.Length);
+                }
+                catch
+                {
+                    break;
+                }
+
+                if (bytesRead == 0)
+                    break;
+
+                string message = Encoding.UTF8.GetString(messageBytes, 0, bytesRead);
+                Console.WriteLine(message);
+            }
+        });
+
+        receiveThread.Start();
+    }
+
+    static void SendMessage(string message)
+    {
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        stream.Write(messageBytes, 0, messageBytes.Length);
+        stream.Flush();
+    }
+}
---
+++ server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    // 😁 list of stupied clints 
+
+    static List<TcpClient> clients = new List<TcpClient>();
+    static TcpListener server; // step one 
+
+    static void Main()
+    {
+        StartServer();
+    }
+
+    static void StartServer()
+    {
+        server = new TcpListener(IPAddress.Any, 8000);
+        server.Start();
+        Console.WriteLine("\n\t\t\t hi kheder \n");
+        Console.WriteLine("  am the sever now am listening for clients connections on port 8000  ******* : ");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient(); 
+            clients.Add(client);
+
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
+
+            // we got this from the enternet (thanks for AI ) 😁😁😁
+
+            Console.WriteLine($"Client connected from {((IPEndPoint)client.Client.RemoteEndPoint).Address}:{((IPEndPoint)client.Client.RemoteEndPoint).Port}");
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient tcpClient = (TcpClient)obj;
+        NetworkStream clientStream = tcpClient.GetStream();
+
+        while (true)
+        {
+            byte[] messageBytes = new byte[4096];
+            int bytesRead;
+
+            try
+            {
+                bytesRead = clientStream.Read(messageBytes, 0, messageBytes.Length);
+            }
+            catch
+            {
+                break;
+            }
+
+            if (bytesRead == 0)
+                break;
+
+            string message = Encoding.UTF8.GetString(messageBytes, 0, bytesRead);
+            BroadcastMessage(message, tcpClient);
+            Console.WriteLine($" a message frome  <{((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address}:{((IPEndPoint)tcpClient.Client.RemoteEndPoint).Port}>: {message}");
+        }
+        // we should remove the stupied clints from the list  after they leave
+        clients.Remove(tcpClient);
+        tcpClient.Close();
+    }
+
+    static void BroadcastMessage(string message, TcpClient excludeClient = null)
+    {
+        foreach (TcpClient client in clients)
+        {
+            if (client != excludeClient)
+            {
+                NetworkStream clientStream = client.GetStream();
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                clientStream.Write(messageBytes, 0, messageBytes.Length);
+                clientStream.Flush();
+            }
+        }
+    }
+}
